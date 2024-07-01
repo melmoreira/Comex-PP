39dd98933ac63b9211c38b498df96fca79eeaa2f
Melissa Maria <107822804+melmoreira@users.noreply.github.com>
2024-07-01 20:31:21 -0300

    extraindo meethodos

--- Comex/Program.cs
+++ Comex/Program.cs
@@ -72,141 +72,37 @@ async Task exibirMenuPrincipal()
     switch (opcaoEscolhidaNumerica)
     {
         case 1:
-            Console.Clear();
-            Console.WriteLine("Registro de Produto");
-
-            Console.Write("Digite o nome do Produto: ");
-            string nomeDoProduto = Console.ReadLine();
-            var produto = new Produto(nomeDoProduto);
-
-            Console.Write("Digite a descrição do Produto: ");
-            string descricaoDoProduto = Console.ReadLine();
-            produto.Descricao = descricaoDoProduto;
-
-            Console.Write("Digite o preço do Produto: ");
-            string preçoDoProduto = Console.ReadLine();
-            produto.PrecoUnitario = double.Parse(preçoDoProduto);
-
-            Console.Write("Digite a quantidade do Produto: ");
-            string quantidadeDoProduto = Console.ReadLine();
-            produto.Quantidade = int.Parse(quantidadeDoProduto);
-
-            listaDeProdutos.Add(produto);
-            Console.WriteLine($"O Produto {produto.Nome} foi registrado com sucesso!");
-            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
+            criarProduto();
+            esperarUser();
             await exibirMenuPrincipal();
             break;
         case 2:
-            Console.Clear();
-
-            ExibirTituloDaOpcao("Exibindo todos os produtos registradoss na nossa aplicação");
-
-            for (int i = 0; i < listaDeProdutos.Count; i++)
-            {
-                Console.WriteLine($"Produto: {listaDeProdutos[i].Nome}, Preço: {listaDeProdutos[i].PrecoUnitario:F2}");
-            }
-
-            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
+            listarProdutos();
+            esperarUser();
             await exibirMenuPrincipal();
             break;
         case 3:
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    Console.Clear();
-                    Console.WriteLine("\nExibindo Produtos\n");
-                    string resposta = await client.GetStringAsync("http://fakestoreapi.com/products");
-                    var produtos = JsonSerializer.Deserialize<List<Produto>>(resposta)!;
-                    for (int i = 0; i < produtos.Count; i++)
-                    {
-                        Console.WriteLine($"Nome: {produtos[i].Nome}, " +
-                            $"Descrição: {produtos[i].Descricao}, " +
-                            $"Preço {produtos[i].PrecoUnitario} \n");
-                    }
-                    esperarUser();
-                    await exibirMenuPrincipal();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Temos um problema: {ex.Message}");
-                }
-            }
+            await consultarApiExternaAsync();
+            esperarUser();
+            await exibirMenuPrincipal();
             break;
         case 4:
-            var produtosOrdenados = listaDeProdutos.OrderBy(p => p.Nome).ToList();
-            Console.Clear();
-            Console.WriteLine("Produtos ordenados pelo título:");
-            for (int i = 0; i < produtosOrdenados.Count; i++)
-            {
-                Console.WriteLine($"Produto: {produtosOrdenados[i].Nome}, Preço: {produtosOrdenados[i].PrecoUnitario:F2}");
-            }
+            listarPorTitulo();
             esperarUser();
             await exibirMenuPrincipal();
             break;
         case 5:
-            var produtosOrdenadosPorPreco = listaDeProdutos.OrderBy(p => p.PrecoUnitario).ToList();
-            Console.Clear();
-            Console.WriteLine("Produtos ordenados pelo preço:");
-            for (int i = 0; i < produtosOrdenadosPorPreco.Count; i++)
-            {
-                Console.WriteLine($"Produto: {produtosOrdenadosPorPreco[i].Nome}, Preço: {produtosOrdenadosPorPreco[i].PrecoUnitario:F2}");
-            }
+            listarPorPreco();
             esperarUser();
             await exibirMenuPrincipal();
             break;
         case 6:
-            Console.Clear();
-            Console.WriteLine("Criando um novo pedido\n");
-
-            Console.Write("Digite o nome do cliente: ");
-            string nomeCliente = Console.ReadLine()!;
-            var cliente = new Cliente();
-            cliente.Nome = nomeCliente;
-
-            var pedido = new Pedido(cliente);
-
-            Console.WriteLine("\nProdutos disponíveis:");
-            for (int i = 0; i < listaDeProdutos.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {listaDeProdutos[i].Nome}");
-            }
-
-            Console.Write("Digite o número do produto que deseja adicionar (ou 0 para finalizar): ");
-            int numeroProduto = int.Parse(Console.ReadLine()!);
-
-            var produtoEscolhido = listaDeProdutos[numeroProduto - 1];
-
-            Console.Write("Digite a quantidade: ");
-            int quantidade = int.Parse(Console.ReadLine()!);
-
-            var itemDePedido = new ItemDePedido(produtoEscolhido, quantidade, produtoEscolhido.PrecoUnitario);
-            pedido.AdicionarItem(itemDePedido);
-
-            Console.WriteLine($"Item adicionado: {itemDePedido}\n");
-
-
-            listaDePedidos.Add(pedido);
-            Console.WriteLine($"\nPedido criado com sucesso:\n{pedido}");
+            criarPedido();
             esperarUser();
             await exibirMenuPrincipal();
             break;
         case 7:
-            Console.Clear();
-            Console.WriteLine("Exibindo todos os produtos registradoss na nossa aplicação");
-
-            var pedidosOrdenados = listaDePedidos.OrderBy(p => p.Cliente.Nome).ToList();
-
-            foreach (var Pedido in pedidosOrdenados)
-            {
-                Console.WriteLine($"Cliente: {Pedido.Cliente.Nome}, Total: {Pedido.Total:F2}");
-            }
-
-            esperarUser();
+            exibirPedidos();
             await exibirMenuPrincipal();
             break;
         case -1:
@@ -216,6 +112,139 @@ async Task exibirMenuPrincipal()
             Console.WriteLine("Opção inválida");
             break;
     }
+}
+
+void criarProduto()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Produto");
+
+    Console.Write("Digite o nome do Produto: ");
+    string nomeDoProduto = Console.ReadLine();
+    var produto = new Produto(nomeDoProduto);
+
+    Console.Write("Digite a descrição do Produto: ");
+    string descricaoDoProduto = Console.ReadLine();
+    produto.Descricao = descricaoDoProduto;
+
+    Console.Write("Digite o preço do Produto: ");
+    string preçoDoProduto = Console.ReadLine();
+    produto.PrecoUnitario = double.Parse(preçoDoProduto);
+
+    Console.Write("Digite a quantidade do Produto: ");
+    string quantidadeDoProduto = Console.ReadLine();
+    produto.Quantidade = int.Parse(quantidadeDoProduto);
+
+    listaDeProdutos.Add(produto);
+    Console.WriteLine($"O Produto {produto.Nome} foi registrado com sucesso!");
+}
+
+void listarProdutos()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibindo todos os produtos registradoss na nossa aplicação");
+
+    for (int i = 0; i < listaDeProdutos.Count; i++)
+    {
+        Console.WriteLine($"Produto: {listaDeProdutos[i].Nome}, Preço: {listaDeProdutos[i].PrecoUnitario:F2}");
+    }
+}
+
+async Task consultarApiExternaAsync()
+{
+    using (HttpClient client = new HttpClient())
+    {
+        try
+        {
+            Console.Clear();
+            Console.WriteLine("\nExibindo Produtos\n");
+            string resposta = await client.GetStringAsync("http://fakestoreapi.com/products");
+            var produtos = JsonSerializer.Deserialize<List<Produto>>(resposta)!;
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                Console.WriteLine($"Nome: {produtos[i].Nome}, " +
+                    $"Descrição: {produtos[i].Descricao}, " +
+                    $"Preço {produtos[i].PrecoUnitario} \n");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Temos um problema: {ex.Message}");
+        }
+    }
+}
+
+void listarPorTitulo()
+{
+    var produtosOrdenados = listaDeProdutos.OrderBy(p => p.Nome).ToList();
+    Console.Clear();
+    Console.WriteLine("Produtos ordenados pelo título:");
+    for (int i = 0; i < produtosOrdenados.Count; i++)
+    {
+        Console.WriteLine($"Produto: {produtosOrdenados[i].Nome}, Preço: {produtosOrdenados[i].PrecoUnitario:F2}");
+    }
+}
+
+void listarPorPreco()
+{
+    var produtosOrdenadosPorPreco = listaDeProdutos.OrderBy(p => p.PrecoUnitario).ToList();
+    Console.Clear();
+    Console.WriteLine("Produtos ordenados pelo preço:");
+    for (int i = 0; i < produtosOrdenadosPorPreco.Count; i++)
+    {
+        Console.WriteLine($"Produto: {produtosOrdenadosPorPreco[i].Nome}, Preço: {produtosOrdenadosPorPreco[i].PrecoUnitario:F2}");
+    }
+}
+
+void criarPedido()
+{
+    Console.Clear();
+    Console.WriteLine("Criando um novo pedido\n");
+
+    Console.Write("Digite o nome do cliente: ");
+    string nomeCliente = Console.ReadLine()!;
+    var cliente = new Cliente();
+    cliente.Nome = nomeCliente;
+
+    var pedido = new Pedido(cliente);
+
+    Console.WriteLine("\nProdutos disponíveis:");
+    for (int i = 0; i < listaDeProdutos.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {listaDeProdutos[i].Nome}");
+    }
+
+    Console.Write("Digite o número do produto que deseja adicionar (ou 0 para finalizar): ");
+    int numeroProduto = int.Parse(Console.ReadLine()!);
+
+    var produtoEscolhido = listaDeProdutos[numeroProduto - 1];
+
+    Console.Write("Digite a quantidade: ");
+    int quantidade = int.Parse(Console.ReadLine()!);
+
+    var itemDePedido = new ItemDePedido(produtoEscolhido, quantidade, produtoEscolhido.PrecoUnitario);
+    pedido.AdicionarItem(itemDePedido);
+
+    Console.WriteLine($"Item adicionado: {itemDePedido}\n");
+
+
+    listaDePedidos.Add(pedido);
+    Console.WriteLine($"\nPedido criado com sucesso:\n{pedido}");
+}
+
+void exibirPedidos()
+{
+    Console.Clear();
+    Console.WriteLine("Exibindo todos os produtos registradoss na nossa aplicação");
+
+    var pedidosOrdenados = listaDePedidos.OrderBy(p => p.Cliente.Nome).ToList();
+
+    foreach (var Pedido in pedidosOrdenados)
+    {
+        Console.WriteLine($"Cliente: {Pedido.Cliente.Nome}, Total: {Pedido.Total:F2}");
+    }
+
+    esperarUser();
 }
 
 void esperarUser()
